442029e22bd785a0043b1f224cd1c223b08a454a
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-02-10 18:23:40 +0530

    #include<stdio.h> int main() { 	int r,i,j; 	scanf("%d",&r); 	int arr[r][r]; 	for(i=0;i<r;i++) 	{ 		for(j=0;j<r;j++) 		{ 			if(i==j) 			{ 				printf("0"); 		    } 			else 			{ 				printf("x"); 			} 		} 		printf(" "); 	} }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -1,15 +1,23 @@
 #include<stdio.h>
 int main()
 {
-    int a,i,j;
-    scanf("%d",&a);
-    for(i=1;i<=a;i++)
-    {
-        for(j=1;j<=i;j++)
-        {
-        printf("%d",j);
-        }
-        printf("
-");
-    }
+	int r,i,j;
+	scanf("%d",&r);
+	int arr[r][r];
+	for(i=0;i<r;i++)
+	{
+		for(j=0;j<r;j++)
+		{
+			if(i==j)
+			{
+				printf("0");
+		    }
+			else
+			{
+				printf("x");
+			}
+		}
+		printf("
+");    
+	}
 }
