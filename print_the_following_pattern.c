ccddeffab39f658a36933e69dc9cc8e80025e54e
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-02-10 18:49:14 +0530

    #include<stdio.h> int main() { 	int r,i,j; 	scanf("%d",&r); 	int arr[r][r]; 	for(i=0;i<r;i++) 	{ 		for(j=0;j<r;j++) 		{ 			if(i==0) 			{ 				printf("A "); 		    } 			else if(i==1) 			{ 				printf("B "); 			} 			else if(i==2) 			{printf("C "); 			} 			else if(i==3) 			{printf("D "); 			} 			else if(i==4) 			{printf("E "); 			} 			else if(i==5) 			{printf("F "); 			} 			else if(i==6) 			{printf("G "); 			} 			else if(i==7) 			{printf("H "); 			} 			else if(i==8) 			{printf("I "); 			} 			else if(i==9) 			{printf("J "); 			} 			else if(i==10) 			{printf("K "); 			} 			else if(i==11) 			{printf("L "); 			} 			else if(i==12) 			{printf("M "); 			} 			else if(i==13) 			{printf("N "); 			} 			else if(i==14) 			{printf("O "); 			} 			else if(i==15) 			{printf("P "); 			} 			else if(i==16) 			{printf("Q "); 			} 			else if(i==17) 			{printf("R "); 			} 			else if(i==18) 			{printf("S "); 			} 			else if(i==19) 			{printf("T "); 			} 			else if(i==20) 			{printf("U "); 			} 			else if(i==21) 			{printf("V "); 			} 			else if(i==22) 			{printf("W "); 			} 			else if(i==23) 			{printf("X "); 			} 			else if(i==24) 			{printf("Y "); 			} 			else if(i==25) 			{printf("Z"); 			} 		} 		printf(" "); 	} }

--- print_the_following_pattern.c
+++ print_the_following_pattern.c
@@ -8,13 +8,85 @@ int main()
 	{
 		for(j=0;j<r;j++)
 		{
-			if(i==j)
+			if(i==0)
 			{
-				printf("0");
+				printf("A ");
 		    }
-			else
+			else if(i==1)
 			{
-				printf("x");
+				printf("B ");
+			}
+			else if(i==2)
+			{printf("C ");
+			}
+			else if(i==3)
+			{printf("D ");
+			}
+			else if(i==4)
+			{printf("E ");
+			}
+			else if(i==5)
+			{printf("F ");
+			}
+			else if(i==6)
+			{printf("G ");
+			}
+			else if(i==7)
+			{printf("H ");
+			}
+			else if(i==8)
+			{printf("I ");
+			}
+			else if(i==9)
+			{printf("J ");
+			}
+			else if(i==10)
+			{printf("K ");
+			}
+			else if(i==11)
+			{printf("L ");
+			}
+			else if(i==12)
+			{printf("M ");
+			}
+			else if(i==13)
+			{printf("N ");
+			}
+			else if(i==14)
+			{printf("O ");
+			}
+			else if(i==15)
+			{printf("P ");
+			}
+			else if(i==16)
+			{printf("Q ");
+			}
+			else if(i==17)
+			{printf("R ");
+			}
+			else if(i==18)
+			{printf("S ");
+			}
+			else if(i==19)
+			{printf("T ");
+			}
+			else if(i==20)
+			{printf("U ");
+			}
+			else if(i==21)
+			{printf("V ");
+			}
+			else if(i==22)
+			{printf("W ");
+			}
+			else if(i==23)
+			{printf("X ");
+			}
+			else if(i==24)
+			{printf("Y ");
+			}
+			else if(i==25)
+			{printf("Z");
 			}
 		}
 		printf("
