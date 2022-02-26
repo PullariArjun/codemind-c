fedacdd88f15d9f443b544c0b7d4ac777c4ed327
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-02-26 17:50:56 +0530

    #include<stdio.h> int main() {     int n,i,ec=0,count=0;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0)         {             ec++;         }     }     for(i=0;i<n;i++)     {         if(arr[i]%2!=0 && i%2!=0)         {             count++;         }     }     if(ec==count)     {         printf("True");     }     else     {         printf("False");     } }

---
+++ Strictly_ODD.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,ec=0,count=0;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0)
+        {
+            ec++;
+        }
+    }
+    for(i=0;i<n;i++)
+    {
+        if(arr[i]%2!=0 && i%2!=0)
+        {
+            count++;
+        }
+    }
+    if(ec==count)
+    {
+        printf("True");
+    }
+    else
+    {
+        printf("False");
+    }
+}
