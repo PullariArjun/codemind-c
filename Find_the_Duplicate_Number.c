3454d474c02c605423059f2c01d3694199f0ea75
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-06-08 12:34:18 +0530

    #include<stdio.h> int main() {     int n,i,j;     scanf("%d",&n);     int arr[n];     for(i=0;i<n;i++)     {         scanf("%d",&arr[i]);     }     for(i=0;i<n;i++)     {int count=0;         for(j=0;j<n;j++)         {             if(arr[i]==arr[j])             {                 count++;             }         }         if(count==2)         {             printf("%d",arr[i]);             break;         }     } }

---
+++ Find_the_Duplicate_Number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main()
+{
+    int n,i,j;
+    scanf("%d",&n);
+    int arr[n];
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&arr[i]);
+    }
+    for(i=0;i<n;i++)
+    {int count=0;
+        for(j=0;j<n;j++)
+        {
+            if(arr[i]==arr[j])
+            {
+                count++;
+            }
+        }
+        if(count==2)
+        {
+            printf("%d",arr[i]);
+            break;
+        }
+    }
+}
