164d1973d3fa3beb275746d072e7543ec2934df0
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2022-06-08 12:19:05 +0530

    #include<stdio.h> int main() {     int n,i;     scanf("%d",&n);     long long int arr[n],sum=0;     for(i=0;i<n;i++)     {         scanf("%lld",&arr[i]);         sum=sum+arr[i];     }     printf("%lld",sum); }

---
+++ A_Very_Big_Sum.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+    int n,i;
+    scanf("%d",&n);
+    long long int arr[n],sum=0;
+    for(i=0;i<n;i++)
+    {
+        scanf("%lld",&arr[i]);
+        sum=sum+arr[i];
+    }
+    printf("%lld",sum);
+}
