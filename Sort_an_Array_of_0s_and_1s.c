5ac744913d8be3f9377071febf4ac1905f508c1e
PullariArjun <98091330+PullariArjun@users.noreply.github.com>
2023-01-10 14:34:18 +0530

    #include<iostream> using namespace std; int main(){     int n,i,j,temp;     cin>>n;     int arr[n];     for(i=0;i<n;i++){         cin>>arr[i];     }     for(i=0;i<n;i++){         for(j=0;j<n;j++){             if(arr[j]>arr[j+1]){                 temp=arr[j];                 arr[j]=arr[j+1];                 arr[j+1]=temp;             }         }     }     for(i=0;i<n;i++){         cout<<arr[i]<<" ";     } }

---
+++ Sort_an_Array_of_0s_and_1s.c
@@ -0,0 +1,22 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int n,i,j,temp;
+    cin>>n;
+    int arr[n];
+    for(i=0;i<n;i++){
+        cin>>arr[i];
+    }
+    for(i=0;i<n;i++){
+        for(j=0;j<n;j++){
+            if(arr[j]>arr[j+1]){
+                temp=arr[j];
+                arr[j]=arr[j+1];
+                arr[j+1]=temp;
+            }
+        }
+    }
+    for(i=0;i<n;i++){
+        cout<<arr[i]<<" ";
+    }
+}
